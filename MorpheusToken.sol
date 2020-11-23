cea70fb2410215a53fe9bb450fe6672d03096fac
Morpheus KungFu Master <73902619+MorpheusGame@users.noreply.github.com>
2020-11-23 13:32:54 +0100

    Update MorpheusToken.sol

--- MorpheusToken.sol
+++ MorpheusToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.5.0;
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "./ERC20.sol";
 import "./ERC20Detailed.sol";
-import "./ERC20Mintable.sol";
 import "./ERC20Capped.sol";
 
 contract MorpheusToken is ERC20, ERC20Detailed, ERC20Capped {
