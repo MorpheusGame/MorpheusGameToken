c8f8372c4fdcdf709527058b79c1ee218136db32
Morpheus KungFu Master <73902619+MorpheusGame@users.noreply.github.com>
2020-11-28 15:44:13 +0100

    Update MorpheusToken.sol

--- MorpheusToken.sol
+++ MorpheusToken.sol
@@ -9,8 +9,11 @@ contract MorpheusToken is ERC20, ERC20Detailed, ERC20Capped {
     address public deployerAddress;
     address public gameControllerAddress;
     
+    bool public locked; 
+    
     constructor(address _deployer) public ERC20Detailed("MorpheusGameToken", "MGT", 18) ERC20Capped(500000000*1E18) {
               deployerAddress =_deployer;
+              locked = true;
     }
 
     modifier onlyGameController() {
@@ -28,6 +31,34 @@ contract MorpheusToken is ERC20, ERC20Detailed, ERC20Capped {
     function eraseDeployerAddress() public onlyDeployer(){
         deployerAddress = address(0x0);
     }
+    
+    function unlock() public onlyDeployer {
+        locked = false;
+    } 
+
+    function lock() public onlyDeployer {
+        locked = true;
+    }
+    
+    function transfer(address to, uint256 amount) public returns(bool) {
+        if(locked) {
+            require(msg.sender == deployerAddress);
+            super.transfer(to, amount);
+        } else{
+            super.transfer(to, amount);
+        }
+
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public returns(bool) {
+        if(locked) {
+            require(msg.sender == deployerAddress);
+            super.transferFrom(from, to, amount);
+        } else{
+            super.transferFrom(from, to, amount); 
+        }
+
+    }
 
     function setGameControllerAddress(address _gameAddress) public onlyDeployer {
         gameControllerAddress = _gameAddress;
@@ -39,8 +70,8 @@ contract MorpheusToken is ERC20, ERC20Detailed, ERC20Capped {
 
     // This is the function used by the gameController Contract for minting token who will be send to the user
     // ONLY GameController can call this function.
-    // AND the gameController have only one reference to THIS function (line 256 in GameController.sol):
-    // In the __callback()  (line 226 in GameController.sol) 
+    // AND the gameController have only one reference to THIS function (line 288 in GameController.sol):
+    // In the __callback()  (line 228 in GameController.sol) 
     // This __callback() function can only be called by provableAPI Address. This meens that only return of ORACLE can return a token minting
     function mintTokensForWinner(uint256 _amount) public onlyGameController() {
         _mint(gameControllerAddress, _amount);
