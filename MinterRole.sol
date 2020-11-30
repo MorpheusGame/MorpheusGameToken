c23d607706016350bcc5f47bea41905968658b5d
Morpheus KungFu Master <73902619+MorpheusGame@users.noreply.github.com>
2020-11-30 08:19:21 +0100

    Update MinterRole.sol

--- MinterRole.sol
+++ MinterRole.sol
@@ -24,6 +24,9 @@ contract MinterRole is Context {
         return _minters.has(account);
     }
 
+    function addMinter(address account) public onlyMinter {
+        _addMinter(account);
+    }
 
     function renounceMinter() public {
         _removeMinter(_msgSender());
